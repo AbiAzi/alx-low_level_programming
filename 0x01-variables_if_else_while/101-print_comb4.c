7edcd5185d3e286e295c1fed069a167b2271aeb7
Abiel Azie <derbewazie@gmail.com>
2022-09-10 03:26:29 +0300

    task 11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,20 +12,20 @@ int main(void)
 		{
 		for (m = 49; m < 58; m++)
 		{
-		for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
+		  for (l = 50; l < 58; l++)
+		{
+			if (l > m && m > n)
 					{
 						putchar(n);
 						putchar(m);
 						putchar(1);
-				if (n != 55 || m != 56)
+			if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 					}
-			}
+		}
 		}
 		}
 		putchar('\n');
