1ca620de9a750f6450f7ae208b9f36cda92d7a5e
Abiel Azie <derbewazie@gmail.com>
2022-09-14 00:39:35 +0300

    task 8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,7 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-
 	while (i < 24)
-	
 	{
 		j = 0;
 		while (j < 60)
@@ -22,11 +20,12 @@ void jack_bauer(void)
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');												_putchar((j % 10) + '0');												_putchar('\n');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 			j++;
   		}
 			i++;
-
 	}
 
 
