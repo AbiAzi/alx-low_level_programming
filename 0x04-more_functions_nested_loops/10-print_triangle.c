3b20e533a544d9241370d7735a50fc8f78f37d38
Abiel Azie <derbewazie@gmail.com>
2022-09-17 15:36:05 +0300

    task 10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
- *
- * Return: Always 0
+ * main - check the code
+ * Return: Always 0.
  */
 
 void print_triangle(int size)
