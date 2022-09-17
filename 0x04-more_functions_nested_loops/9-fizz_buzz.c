f531a7fcc3c741ff78dc568c814e9e81650e05c3
Abiel Azie <derbewazie@gmail.com>
2022-09-17 12:45:04 +0300

    task 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,21 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ *
+ * Return: 0
  */
 void print_triangle(int size)
 {
 	int i, j;
 	for (size > 0)
 	{
-	for (i = 1; j <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-	for ((i = size - i); j > 0; j--)
+	for ((j = size - i); j > 0; j--)
 	{
 	putchar('');
 	}
