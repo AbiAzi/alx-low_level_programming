cb70fad199420720c4ee440e7d0b705b9ea0fbf2
Abiel Azie <derbewazie@gmail.com>
2022-10-07 12:27:23 +0300

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,32 +20,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		unsigned int concat_len;
 		char *concat;
 
-			s1_len = 0;
+		s1_len = 0;
 
-			if (s1 == NULL)
-			s1 = "";
-			if (s2 == NULL)
-			s2 = "";
+		if (s1 == NULL)
+		s1 = "";
 
-			for (i = 0; s1[i]; i++)
-			s1_len++;
+		if (s2 == NULL)
+		s2 = "";
 
-			concat_len = s1_len + n;
+		for (i = 0; s1[i]; i++)
+		s1_len++;
 
-			concat = malloc(sizeof(char) * (concat_len + 1));
-			if (concat == NULL)
-			return (NULL);
-			for (i = 0; i < s1_len; i++)
-			{
-			concat[i] = s1[i];
-			}
+		concat_len = s1_len + n;
 
-			for (j = 0; j < n; j++, i++)
-			{
-			concat[i] = s2[j];
-			}
+		concat = malloc(sizeof(char) * (concat_len + 1));
+		if (concat == NULL)
+		return (NULL);
 
-			concat[i] = '\0';
+		for (i = 0; i < s1_len; i++)
+		{
+		concat[i] = s1[i];
+		}
 
-			return (concat);
+		for (j = 0; j < n; j++, i++)
+		{
+		concat[i] = s2[j];
+		}
+
+		concat[i] = '\0';
+
+		return (concat);
 }
