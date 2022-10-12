8c34ae7610a23ab2145325a0401d0f7f161e65df
Abiel Azie <111020887+AbiAzi@users.noreply.github.com>
2022-10-12 22:53:18 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,80 +1,61 @@
 #include "3-calc.h"
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 
 /**
- * op_add - function returns sum
- * @a: first argument
- * @b: second argument
- * Return: a + b
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
-/**
- * op_sub - function returns difference
- * @a: first argument
- * @b: second argument
- * Return: a - b
- */
-int op_sub(int a, int b)#include "3-calc.h"
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 
 /**
- * op_add - function returns sum
- * @a: first argument
- * @b: second argument
- * Return: a + b
- */
-int op_add(int a, int b)
-{
-return (a + b);
-}
-/**
- * op_sub - function returns difference
- * @a: first argument
- * @b: second argument
- * Return: a - b
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
+
 /**
- * op_mul - function returns product
- * @a: first argument
- * @b: second argument
- * Return: a * b
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
+
 /**
- * op_div - function returns quitient
- * @a: first agument
- * @b: second argument
- * Return: a / b
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
+
 /**
- * op_mod - function returns remainder
- * @a: first argumet
- * @b: second argument
- * Return: a % b
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
