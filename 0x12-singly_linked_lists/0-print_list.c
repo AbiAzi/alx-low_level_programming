09cfd9a60c06d736c182205e63cebeedb7c8b73a
Abiel Azie <derbewazie@gmail.com>
2022-10-21 00:22:57 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of list_t
+ * @h: a pointer to the head node
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+	size_t count = 0;
+	const list_t *temp = NULL;
+
+	temp = h;
+	for (i = 0; temp != NULL; i++)
+	{
+	if (temp->str == NULL)
+	printf("[0] (nil)\n");
+	else
+	printf("[%d] %s\n", temp->len, temp->str);
+	count++;
+	temp = temp->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - function adds new node at the beginning of the list
+ * @head: pointer to the head
+ * @str: the new node to added
+ * Return: the address of the new element or NULL on failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+		int i, count = 0;
+		list_t *new = NULL;
+
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+		return (NULL);
+		for (i = 0; str[i]; i++)
+		count++;
+		new->str = strdup(str);
+		new->len = count;
+		new->next = *head;
+		*head = new;
+		return (*head);
+}
